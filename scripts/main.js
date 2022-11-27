8eec5af62220614b9ad347019ef11676eea266c2
pan <2mindwolf@gmail.com>
2022-11-28 00:16:17 +0700

    day 1

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-var visibleChunk = 0
+var visibleChunk = []
 var allChunk = []
 var player = 0
 
@@ -7,9 +7,9 @@ function start() {
     let screen = document.getElementById("screen")
     for (let i = 0; i < 81; i++) {
         let block = document.createElement("div")
-        block.classList.add("blocks")
+        block.className = "blocks"
 
-        block.id = "block(" + i % 9 + "," + Math.floor(i / 9) + ")"
+        block.id = "block(" + Math.floor(i / 9) + "," + i % 9 + ")"
 
         screen.appendChild(block)
     }
@@ -17,26 +17,53 @@ function start() {
     //create visible chunk
     let chunk = new Chunk(0, 0)
     allChunk.push(chunk)
-    visibleChunk = chunk.chunkMap
+    for (let y = 0; y < 9; y++) {
+        visibleChunk.push(chunk.chunkMap[y].slice())
+    }
 
     //create player
     player = new Player()
     
+    console.log(visibleChunk)
     visibleChunk[player.y][player.x] = 1
 
     console.table(visibleChunk)
 
     display()
+
+    //keyboard input
+    window.addEventListener("keydown", (key) => {keydown(key)})
+}
+function keydown(key) {
+    switch (key.key) {
+        case 'w':
+            walk(-1, 0)
+            break
+        case 's':
+            walk(1, 0)
+            break
+        case 'a':
+            walk(0, -1)
+            break
+        case 'd':
+            walk(0, 1)
+            break
+        default:
+            console.log(key)
+            break
+    }
 }
 function display() {
     let numtoclass = {
         0 : "grass",
         1 : "player"
     }
-    for (let y = 0; y < 9; y++) {
-        for (let x = 0; x < 9; x++) {
-            let id = "block(" + x + "," + y + ")"
-            document.getElementById(id).classList.add(numtoclass[visibleChunk[y][x]])   
+    for (let y = 8; y > -1; y--) {
+        for (let x = 8; x > -1; x--) {
+            let id = "block(" + y + "," + x + ")"
+            let block = document.getElementById(id)
+            block.className = "blocks"
+            block.classList.add(numtoclass[visibleChunk[y][x]])   
         }
     }
 }
--- scripts/player.js
+++ scripts/player.js
@@ -1,6 +1,47 @@
+function walk(y, x) {
+    player.move(y, x)
+    let chunk = player.currentChunk()
+    for (let y = 0; y < 9; y++) {
+        visibleChunk[y] = chunk.chunkMap[y].slice()
+    }
+    visibleChunk[player.y][player.x] = 1
+
+    document.getElementById("player-pos").innerHTML = "pos : (" + player.x + ", " + player.y + ")"
+    document.getElementById("chunk-pos").innerHTML = "chunk pos : (" + player.chunkX + ", " + player.chunkY + ")"
+
+    display()
+}
 class Player {
     constructor() {
         this.x = 4
         this.y = 4
+        this.chunkX = 0
+        this.chunkY = 0
+    }
+    move(y, x) {
+        this.x = this.x + x
+        this.y = this.y + y
+
+        if (this.x < 0 || this.x > 8 || this.y < 0 || this.y > 8) {
+            this.moveChunk(this.y, this.x)
+        }
+    }
+    moveChunk(y, x) {
+        if (x < 0 || x > 8) {
+            this.chunkX = this.chunkX + ((x > 8) ? 1 : -1)
+            this.x = (x > 8) ? 0 : 8
+        } else {
+            this.chunkY = this.chunkY + ((y > 8) ? 1 : -1)
+            this.y = (y > 8) ? 0 : 8
+        }
+    }
+    currentChunk() {
+        for (let i = 0; i < allChunk.length; i++) {
+            if (allChunk[i].y == this.chunkY && allChunk[i].x == this.chunkX) {
+                return allChunk[i]
+            }
+        }
+        let chunk = new Chunk(this.chunkX, this.chunkY)
+        return chunk
     }
 }
